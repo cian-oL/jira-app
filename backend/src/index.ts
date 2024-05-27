3ed7ebb509564a8474fa63c3c0fa6cf824d33b1b
mecastor <mecastor87@gmail.com>
2024-05-27 22:27:47 +0100

    Initial backend setup

---
+++ backend/src/index.ts
@@ -0,0 +1,19 @@
+import express, { Request, Response } from "express";
+import cors from "cors";
+import "dotenv/config";
+import mongoose from "mongoose";
+
+const MONGO_DB_CONNECTION_STRING = process.env.MONGO_DB_CONNECTION_STRING;
+const PORT = process.env.PORT;
+
+mongoose
+  .connect(MONGO_DB_CONNECTION_STRING as string)
+  .then(() => console.log("Successful connection to database"))
+  .catch((err) => console.error("Unsuccessful connection to database", err));
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+app.listen(PORT, () => console.log(`Backend server listening on port ${PORT}`));
