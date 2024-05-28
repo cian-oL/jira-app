abec07e8564086a39e9b72812dd139a7cf124756
mecastor <mecastor87@gmail.com>
2024-05-28 13:09:27 +0100

    Added auth0 login and provider functionailty and added queryclientprovider for query hooks

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,14 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "react-query";
 
 import App from "./App.tsx";
 import "./index.css";
+import Auth0ProviderWithNavigate from "./auth/Auth0ProviderWithNavigate.tsx";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: true,
+    },
+  },
+});
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <Auth0ProviderWithNavigate>
+          <App />
+        </Auth0ProviderWithNavigate>
+      </QueryClientProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
