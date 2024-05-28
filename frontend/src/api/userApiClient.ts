9cb0dbeea5d5d3c13866d1775b309bbc2eb956a0
mecastor <mecastor87@gmail.com>
2024-05-28 20:18:09 +0100

    Added get and update user fetch requests

--- frontend/src/api/userApiClient.ts
+++ frontend/src/api/userApiClient.ts
@@ -1,10 +1,7 @@
-import { useMutation } from "react-query";
+import { useMutation, useQuery } from "react-query";
+import { useAuth0 } from "@auth0/auth0-react";
 
-type CreateUserData = {
-  auth0Id: string;
-  email: string;
-  name?: string;
-};
+import { User, CreateUserData, UserProfileData } from "@/types/types";
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
@@ -27,4 +24,73 @@ export const useCreateUser = () => {
   const { mutateAsync: createUser } = useMutation(createUserRequest);
 
   return { createUser };
+};
+
+export const useGetCurrentUser = () => {
+  const { getAccessTokenSilently } = useAuth0();
+
+  const getCurrentUserRequest = async (): Promise<User> => {
+    const accessToken = await getAccessTokenSilently();
+
+    const response = await fetch(`${API_BASE_URL}/api/user`, {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error("Error fetching user");
+    }
+
+    return response.json();
+  };
+
+  const {
+    data: currentUser,
+    error,
+    isLoading,
+  } = useQuery("fetchCurrentUser", getCurrentUserRequest);
+
+  if (error) {
+    console.log(error.toString());
+  }
+
+  return { currentUser, isLoading };
+};
+
+export const useUpdateCurrentUser = () => {
+  const { getAccessTokenSilently } = useAuth0();
+
+  const updateCurrentUserRequest = async (userProfileData: UserProfileData) => {
+    const accessToken = getAccessTokenSilently();
+
+    const response = await fetch(`${API_BASE_URL}/api/user`, {
+      method: "PUT",
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(userProfileData),
+    });
+
+    if (!response.ok) {
+      throw new Error("Error updating user");
+    }
+
+    return response.json();
+  };
+
+  const {
+    mutateAsync: updateUser,
+    error,
+    isLoading,
+  } = useMutation(updateCurrentUserRequest);
+
+  if (error) {
+    console.log(error.toString());
+  }
+
+  return { updateUser, isLoading };
 };
