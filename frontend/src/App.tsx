279594efa2c6eb88e6858f6a850172911dc47137
mecastor <mecastor87@gmail.com>
2024-05-28 20:20:04 +0100

    Added UserProfilePage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,8 @@ import { Routes, Route, Navigate } from "react-router-dom";
 
 import Layout from "@/layouts/Layout";
 import AuthCallbackPage from "./pages/AuthCallbackPage";
+import UserProfilePage from "./pages/UserProfilePage";
+import ProtectedRoute from "./auth/ProtectedRoute";
 
 function App() {
   return (
@@ -14,14 +16,16 @@ function App() {
           </Layout>
         }
       />
-      <Route
-        path="/register"
-        element={
-          <Layout>
-            <div>Registration Page</div>
-          </Layout>
-        }
-      />
+      <Route element={<ProtectedRoute />}>
+        <Route
+          path="/profile"
+          element={
+            <Layout>
+              <UserProfilePage />
+            </Layout>
+          }
+        />
+      </Route>
       <Route
         path="/auth-callback"
         element={
